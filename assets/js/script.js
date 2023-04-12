f030a8a872c2fd7fedacd23a664937b53b1e3d1a
Lucas Davi <116373520+LDavi05@users.noreply.github.com>
2023-04-12 07:17:55 -0300

    Atualizar o script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,17 @@ let button = document.querySelector("#generate_btn");
 
 let caracterValue = document.querySelector("#qtde_caracters");
 let password = document.querySelector("#password");
+let radioAll = document.querySelector("#tudo");
+let radioLetters = document.querySelector("#letters");
+let radioNumbers = document.querySelector("#numbers");
+let radioSymbols = document.querySelector("#symbols");
 
 let passwordArea = document.querySelector(".password_area");
 
-let charOptions = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let charOptions = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@?/_-#$*:;";
+let charOptionsLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let charOptionsNumbers = "0123456789";
+let charOptionsSymbols = "!@?/_-#$*:;=%™[](){}";
 let generatedPassword = "";
 
 caracterValue.innerHTML = slider.value;
@@ -19,8 +26,22 @@ slider.oninput = function() {
 const passwordGenerate = () => {
   let pass = "";
   
-  for (let i = 0, n = charOptions.length; i < slider.value; i++) {
-    pass += charOptions.charAt(Math.floor(Math.random() * n));
+  if (radioAll.checked == true) {
+    for (let i = 0, n = charOptions.length; i < slider.value; i++) {
+      pass += charOptions.charAt(Math.floor(Math.random() * n));
+    }
+  } else if (radioLetters.checked == true) {
+    for (let i = 0, n = charOptionsLetters.length; i < slider.value; i++) {
+      pass += charOptionsLetters.charAt(Math.floor(Math.random() * n));
+    }
+  } else if (radioNumbers.checked == true) {
+    for (let i = 0, n = charOptionsNumbers.length; i < slider.value; i++) {
+      pass += charOptionsNumbers.charAt(Math.floor(Math.random() * n));
+    }
+  } else if (radioSymbols.checked == true) {
+    for (let i = 0, n = charOptionsSymbols.length; i < slider.value; i++) {
+      pass += charOptionsSymbols.charAt(Math.floor(Math.random() * n));
+    }
   }
   
   passwordArea.classList.remove("hide");
